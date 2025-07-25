dd849f332a6e7e060e44a74a786e3884e615a43d
5PenguinsInATrenchCoat <draven2121@gmail.com>
2025-07-25 09:11:15 -0700

    Create user.py

---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+  def __init__(self, username, user_id, friend_list, is_account_private = True):
+    self.username = username
+    self.user_id = user_id
+    self.friend_list = friend_list
+    self.is_account_private = is_account_private = is_account_private
