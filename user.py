1e31175247b30f428c1e7ca1095458c1c62c38bb
5PenguinsInATrenchCoat <draven2121@gmail.com>
2025-07-25 09:22:21 -0700

    Update user.py

--- user.py
+++ user.py
@@ -1,6 +1,25 @@
 class User:
-  def __init__(self, username, user_id, friend_list, is_account_private = True):
+  def __init__(self, username, user_id, friend_list, game_list, is_account_private = True):
     self.username = username
     self.user_id = user_id
     self.friend_list = friend_list
+    self.game_list = game_list
     self.is_account_private = is_account_private = is_account_private
+
+def check_privacy_setting(self, user_id, is_account_private):
+  pass
+  #check to see the privacy status of the user profile
+
+def get_game_list(self, user_id, game_list, is_account_private):
+  if self.is_account_private == True:
+    pass
+    #return error and instructions to set your profile to public
+
+def get_friend_list(self, user_id, friend_list, is_account_private):
+  if self.is_account_private == True:
+    pass
+    #return error and instructions to set your profile to public
+
+
+
+
