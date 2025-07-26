2c70c950c5b88aa396d022ae9354720713e36daf
Draven <draven2121@gmail.com>
2025-07-25 19:20:04 -0700

    Started work on privacy check

--- classes/userClass.py
+++ classes/userClass.py
@@ -1,25 +1,43 @@
+from constants import STEAM_ID_LINK, GAME_TAGS_LINK
+from bs4 import BeautifulSoup
+import os
+
+
+
 class User:
-  def __init__(self, username, user_id, friend_list, game_list, is_account_private = True):
-    self.username = username
+  def __init__(self, user_id, friend_list = [], game_list = [], is_account_private = True):
     self.user_id = user_id
     self.friend_list = friend_list
     self.game_list = game_list
     self.is_account_private = is_account_private
 
-def check_privacy_setting(self, user_id, is_account_private):
-  pass
-  #check to see the privacy status of the user profile
+def check_privacy_setting(self, user_id, is_account_private): #check to see the privacy status of the user profile
+    user_id_link = STEAM_ID_LINK + self.user_id
+
+    with open(user_id_link) as fp:
+      soup = BeautifulSoup(fp, 'html.parser')
+    soup = BeautifulSoup(f"<html>{user_id_link}</html>", 'html.parser')
+      
+    info_table = soup.find("table", id = "profile-info")
+    for child in info_table.contents:
+      print(child)
+
+  
 
 def get_game_list(self, user_id, game_list, is_account_private):
-  if self.is_account_private == True:
+  if self.is_account_private == True:   #return error and instructions to set your profile to public
     pass
-    #return error and instructions to set your profile to public
 
 def get_friend_list(self, user_id, friend_list, is_account_private):
-  if self.is_account_private == True:
+  if self.is_account_private == True:   #return error and instructions to set your profile to public
     pass
-    #return error and instructions to set your profile to public
+    
 
 
 
+def main():
+  moogle = User("76561198053719898")
 
+
+if __name__ == "__main__":
+  main()
--- constants.py
+++ constants.py
@@ -1,1 +1,4 @@
+#URLs
+STEAM_ID_LINK = "https://www.steamidfinder.com/lookup/"
+GAME_TAGS_LINK = "https://www.lorenzostanco.com/lab/steam/u/"
 
