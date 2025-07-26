ecec7d01738911f45717ec32140e7d10d6878836
Awowz <tonygriffith75@gmail.com>
2025-07-25 21:55:21 -0500

    added find_common_games function to utilclass alongside testcase. made modifcations to gameClass.py for easy comparisions

--- classes/gameClass.py
+++ classes/gameClass.py
@@ -12,4 +12,16 @@ class Game:
         else:
             self.is_selected = True
 
+    def __eq__(self, other_game):
+        if isinstance(other_game, Game):
+            return self.app_id == other_game.app_id
+        return NotImplementedError
     
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def __hash__(self):
+        return hash((self.app_id, self.name))
--- classes/utilityClass.py
+++ classes/utilityClass.py
@@ -4,8 +4,17 @@ from constants import UPPER_LIMIT, LOWER_LIMIT, UPPER_MAX_BATCH, LOWER_MAX_BATCH
 
 class Utility:    
 
-    def find_common_games(selected_users):
-        pass
+    '''takes primary user, and a group of selected friends (not all friends). return a list of games they have in common'''
+    def find_common_games(main_user: User, selected_friends: list[User]) -> list[Game]:
+        if main_user == None:
+            return None
+        games_shared_with_friends = selected_friends[0].game_list
+        for x in range(0, len(selected_friends)):
+            if selected_friends[x].is_account_private or len(selected_friends[x].game_list) == 0:
+                continue
+            games_shared_with_friends = list(set(games_shared_with_friends).intersection(selected_friends[x].game_list))
+        return games_shared_with_friends
+        
 
     def reduce_games_list_from_genre(filtering_genre, list_of_games):
         pass
